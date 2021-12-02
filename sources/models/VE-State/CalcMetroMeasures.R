742b98aeb2eb2e03607062af3ae1225917ac40a8
Gabrielle Freeman <gabrielle.freeman@rsginc.com>
2021-12-02 10:41:40 -0500

    summary scripts

---
+++ sources/models/VE-State/CalcMetroMeasures.R
@@ -0,0 +1,14 @@
+library(visioneval)
+library(VEReports)
+source("CalcMetroMeasuresFunction.R")
+Ma <- c("Albany", "Bend", "Corvallis", "EugeneSpringfield", "GrantsPass", 
+        "Metro", "RogueValley", "SalemKeizer", "None")
+print("Calculating metropolitan area measures for 2005")
+write.csv(calcMetropolitanMeasures(Year = "2005", Ma = Ma), row.names = FALSE,
+          file = "metro_measures_2005.csv")
+print("Calculating metropolitan area measures for 2010")
+write.csv(calcMetropolitanMeasures(Year = "2010", Ma = Ma), row.names = FALSE,
+          file = "metro_measures_2010.csv")
+print("Calculating metropolitan area measures for 2015")
+write.csv(calcMetropolitanMeasures(Year = "2015", Ma = Ma), row.names = FALSE,
+          file = "metro_measures_2015.csv")
